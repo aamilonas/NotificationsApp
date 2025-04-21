f17f3cae6956d00780b7a8a90892c4505d95e323
aamilonas <angelomilonas1@gmail.com>
2025-04-20 20:59:28 -0400

    Initial Commit

---
+++ NotificationsApp/NotificationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsAppApp.swift
+//  NotificationsApp
+//
+//  Created by Angelo Milonas on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotificationsAppTests/NotificationsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsAppTests.swift
+//  NotificationsAppTests
+//
+//  Created by Angelo Milonas on 4/20/25.
+//
+
+import Testing
+@testable import NotificationsApp
+
+struct NotificationsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
